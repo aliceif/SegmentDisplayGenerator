8c1feb0f8b30cdf65ea535d0fec48bd98fdb7778
aliceif <7098860+aliceif@users.noreply.github.com>
2024-10-06 16:52:23 +0200

    implement image processing

---
+++ src/SegmentedDisplayGenerator.Core/ImageProcessing.cs
@@ -0,0 +1,30 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SegmentedDisplayGenerator.Core;
+
+public static class ImageProcessing
+{
+	public static IEnumerable<PixelPosition> ParsePixels(Image<Rgb24> image)
+	{
+		for (int x = 0; x < image.Width; ++x)
+		{
+			for (int y = 0; y < image.Height; ++y)
+			{
+				if (image[x, y] == new Rgb24(0xff, 0xff, 0xff))
+				{
+					yield return new PixelPosition(x, y);
+				}
+			}
+		}
+	}
+
+	public static Image<Rgb24> CreateDyedImage(Image<Rgb24> image, IEnumerable<IList<PixelPosition>> areas, Rgb24 color)
+	{
+		var output = image.Clone();
+		foreach(var pixel in areas.SelectMany(x => x)){
+			output[pixel.X, pixel.Y] = color;
+		}
+		return output;
+	}
+}
