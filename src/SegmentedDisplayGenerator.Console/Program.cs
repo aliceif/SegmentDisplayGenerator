7368e12cd6a2cbc155ea45f62aa53c7a70600c7f
aliceif <7098860+aliceif@users.noreply.github.com>
2024-10-19 20:06:26 +0200

    implement command line interface

--- src/SegmentedDisplayGenerator.Console/Program.cs
+++ src/SegmentedDisplayGenerator.Console/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+
+using SegmentedDisplayGenerator.Core;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace SegmentedDisplayGenerator.Console;
+
+class Program
+{
+	static async Task<int> Main(string[] args)
+	{
+		var rootCommand = new RootCommand("Program for generating segment display graphics");
+
+		var templateOption = new Option<FileInfo?>(name: "--template", description: "the template file for generating the graphics");
+		var outputOption = new Option<DirectoryInfo?>(name: "--output", description: "the directory to output the generated graphics in");
+		var litOption = new Option<Color?>(name: "--lit-segment-color", description: "the color to fill lit segments",
+		parseArgument: result =>
+		{
+			if (!result.Tokens.Any()) return Color.Red;
+			if (!Color.TryParse(result.Tokens.Single().Value, out var litColor))
+			{
+				result.ErrorMessage = "The color was not recgonized. Try using hex notation like in HTML.";
+				return Color.Red;
+			}
+			else { return litColor; };
+		});
+		var unlitOption = new Option<Color?>(name: "--unlit-segment-color", description: "the color to fill unlit segments",
+		parseArgument: result =>
+		{
+			if (!result.Tokens.Any())
+			{
+				return null;
+			}
+			if (!Color.TryParse(result.Tokens.Single().Value, out var litColor))
+			{
+				result.ErrorMessage = "The color was not recgonized. Try using hex notation like in HTML.";
+				return null;
+			}
+			else
+			{
+				return litColor;
+			};
+		});
+
+		rootCommand.AddOption(templateOption);
+		rootCommand.AddOption(outputOption);
+		rootCommand.AddOption(litOption);
+		rootCommand.AddOption(unlitOption);
+
+		rootCommand.SetHandler(Generate, templateOption, outputOption, litOption, unlitOption);
+
+		return await rootCommand.InvokeAsync(args);
+	}
+
+	private static async Task Generate(FileInfo? template, DirectoryInfo? output, Color? lit, Color? unlit)
+	{
+		System.Console.WriteLine("Processing...");
+		ArgumentNullException.ThrowIfNull(template);
+		ArgumentNullException.ThrowIfNull(output);
+		lit ??= Color.Red;
+		unlit ??= Color.DimGray;
+		var image = await Image.LoadAsync<Rgb24>(template.FullName);
+
+		var areas = AreaFinder.FindAreas(ImageProcessing.ParsePixels(image).ToArray()).ToArray();
+
+		System.Console.WriteLine($"Areas detected: {areas.Length}");
+
+		image = ImageProcessing.CreateDyedImage(image, areas, unlit.Value);
+
+		var areaIndex = 1;
+		var subsets = ListOperations.CreateSubsets(areas).ToArray();
+		foreach (var permutation in subsets)
+		{
+			System.Console.WriteLine($"Creating picture {areaIndex:000}");
+			var activedImage = ImageProcessing.CreateDyedImage(image, permutation, lit.Value);
+			activedImage.SaveAsPng(Path.Join(output.FullName, $"{areaIndex:000}.png"));
+			++areaIndex;
+		}
+
+		System.Console.WriteLine("done.");
+	}
+}
