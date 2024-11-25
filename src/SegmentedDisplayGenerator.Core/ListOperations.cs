ba3b52da5c72d77989ba4f1eb9ea4172a915d850
aliceif <7098860+aliceif@users.noreply.github.com>
2024-11-25 16:50:51 +0100

    add tagged subset type for facilitating structured file name

--- src/SegmentedDisplayGenerator.Core/ListOperations.cs
+++ src/SegmentedDisplayGenerator.Core/ListOperations.cs
@@ -1,12 +1,16 @@
 namespace SegmentedDisplayGenerator.Core;
 
+public record class TaggedSubset<T>(string Tag, IEnumerable<T> Subset);
+
 public static class ListOperations
 {
-	public static IEnumerable<IEnumerable<T>> CreateSubsets<T>(this ICollection<T> items){
-		if (items.Count == 0) return [[]];
-		IEnumerable<IEnumerable<T>> subsets = [ Enumerable.Empty<T>(), [items.First()]];
-		foreach(var item in items.Skip(1)){
-			subsets =  subsets.SelectMany(s => new IEnumerable<T>[]{s, s.Append(item)} );
+	public static IEnumerable<TaggedSubset<T>> CreateSubsets<T>(this ICollection<T> items)
+	{
+		if (items.Count == 0) return [new TaggedSubset<T>("", [])];
+		IEnumerable<TaggedSubset<T>> subsets = [new TaggedSubset<T>("0", []), new TaggedSubset<T>("1", [items.First()])];
+		foreach (var item in items.Skip(1))
+		{
+			subsets = subsets.SelectMany(s => new TaggedSubset<T>[] { s with { Tag = "0" + s.Tag }, s with { Tag = "1" + s.Tag, Subset = s.Subset.Append(item) } });
 		}
 		return subsets;
 	}
