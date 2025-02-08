60815f4db55517dba539d4169cfd24c9dcf98602
aliceif <7098860+aliceif@users.noreply.github.com>
2025-02-08 20:18:16 +0100

    implement MisskeyPackExportWriter

---
+++ src/SegmentedDisplayGenerator.Core/Export/Model/MisskeyEmoji.cs
@@ -0,0 +1,8 @@
+﻿namespace SegmentedDisplayGenerator.Core;
+
+public class MisskeyEmoji
+{
+    public bool Downloaded { get; init; }
+    public required string FileName { get; init; }
+    public required MisskeyEmojiData Emoji { get; init; }
+}
---
+++ src/SegmentedDisplayGenerator.Core/Export/Model/MisskeyMeta.cs
@@ -0,0 +1,12 @@
+﻿namespace SegmentedDisplayGenerator.Core;
+
+public class MisskeyMeta
+{
+	public long MetaVersion { get; init;}
+	public string? Host {get;init;}
+	/// <summary>
+	/// ECMAScript Date.prototype.toString
+	/// </summary>
+	public DateTimeOffset? ExportedAt {get;init;}
+	public List<MisskeyEmoji?> Emojis {get;init;} = [];
+}
